0248c419db920a05ebd161baa52bc9edb161a281
BeCoolFool <zakharovvladbcf@gmail.com>
2020-05-07 20:08:59 +0300

    AM-44
    Set cookies for Cookies disclamer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 import Nav from "./components/Nav/Nav";
 import Footer from "./components/Footer/Footer";
@@ -9,10 +10,27 @@ import SignUpPage from "./pages/SignUpPage";
 import SignInPage from "./pages/SignInPage";
 
 import styles from "./App.module.css";
+import CookiesBanner from "./components/CookiesBanner/CookiesBanner";
 
 const App: React.FC = () => {
+  const [banner, setBanner] = useState<boolean>(false);
+  const [cookies, setCookies] = useCookies(["name"]);
+
+  useEffect(() => {
+    setBanner(!cookies.name);
+  }, [cookies.name]);
+
+  const handleCookies = () => {
+    setCookies("name", "Cookie data replacement", {
+      path: "/",
+      expires: new Date(9999, 11, 11),
+    });
+  };
   return (
     <Router>
+      {banner ? (
+        <CookiesBanner agree={handleCookies} close={setBanner} />
+      ) : null}
       <Nav />
       <div className={styles.container}>
         <div className={styles.content}>
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { CookiesProvider } from 'react-cookie';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <CookiesProvider >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </CookiesProvider>,
   document.getElementById('root')
 );
