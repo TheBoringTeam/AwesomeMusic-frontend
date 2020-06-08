ce055425149430258ce206c2db86dcda5fbd8ec2
HolkerDev <nick.penkovskiy@gmail.com>
2020-06-08 22:27:52 +0200

    Fixed docker script

--- stop.sh
+++ stop.sh
@@ -1,6 +1,1 @@
-if [ ! "$(docker ps -q -f name=front-end)" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name=front-end)" ]; then
-        # cleanup
-        docker rm front-end
-    fi
-fi
+docker stop front-end
